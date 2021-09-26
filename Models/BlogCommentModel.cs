aa1af4dd8fe753f596b8bd530234a5fb56cb82e4
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2021-09-25 23:51:29 -0400

    created models
    created Blog, Comment, Tag, and Post Models

---
+++ Models/BlogCommentModel.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBlogProject.Models
+{
+    public class BlogCommentModel
+    {
+
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        public string AuthorId { get; set; }
+        public string ModeratorId { get; set; }
+
+        [Required]
+        [StringLength(500, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters long.", MinimumLength = 2)]
+        [Display(Name = "Comment")]
+        public string Body { get; set; }
+
+        public DateTime CommentCreated { get; set; }
+        public DateTime? CommentUpdate { get; set; }
+        public DateTime? CommentModerated { get; set; }
+        public DateTime? CommentDeleted { get; set; }
+
+        [StringLength(500, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters long.", MinimumLength = 2)]
+        [Display(Name = "Moderated Comment")]
+        public string CommentModeratedBody { get; set; }
+
+        // Navigation Properties
+        public virtual BlogPostModel Post { get; set; }
+        public virtual IdentityUser Author { get; set; }
+        public virtual IdentityUser Moderator { get; set; }
+        
+
+
+    }
+}
