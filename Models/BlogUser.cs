ce5ec610a8e1d88a009ac9a2174c85ff3f4ef16d
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2021-10-04 22:55:40 -0400

    added BlogUser Model

---
+++ Models/BlogUser.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBlogProject.Models
+{
+    public class BlogUser : IdentityUser
+    {
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters long", MinimumLength = 2)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters long", MinimumLength = 2)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        public byte[] ImageData { get; set; }
+        public string ContentType { get; set; }
+
+
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters long", MinimumLength = 2)]
+        public string FacebookUrl { get; set; }
+
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters long", MinimumLength = 2)]
+        public string TwitterUrl { get; set; }
+
+
+
+    }
+}
